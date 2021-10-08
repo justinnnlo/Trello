840a9e3813e1f7e854f461030ec4ab846f375129
Sam Clark <clarksam19@gmail.com>
2021-10-08 09:44:59 -0500

    editable input toggles based on clicking, enter, and blur

--- client/src/components/newUI/List.jsx
+++ client/src/components/newUI/List.jsx
@@ -3,19 +3,34 @@ import ExistingCards from './ExistingCards';
 
 const List = ({ list }) => {
   const [title, setTitle] = useState(list.title);
+  const [edit, setEdit] = useState(false);
 
+  const handleKeyUp = (e) => {
+    if (e.key === "Enter") {
+      //edit list title with current value of title
+      setEdit(false)
+    }
+  }
+
+  const handleBlur = (e) => {
+
+    setEdit(false)
+  }
   return (
     <div className="list-wrapper">
       <div className="list-background">
-        <div className="list">
+        <div className="list" onClick={(e) => setEdit(true)}>
           <a className="more-icon sm-icon" href=""></a>
           <div>
-            <input
+            {edit ? <input
               type="text"
+              autoFocus
               value={title}
               onChange={(e) => setTitle(e.target.value)}
+              onBlur={handleBlur}
+              onKeyUp={handleKeyUp}
               className="list-title"
-            />
+            /> : <p>{title}</p>}
           </div>
           <ExistingCards listId={list._id} />
           <div className="add-dropdown add-top">
