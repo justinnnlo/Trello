c5e9df52b335e97c6154b0a232c48338da76d674
Sam Clark <clarksam19@gmail.com>
2021-10-08 08:50:04 -0500

    new list renders in component

--- client/src/actions/ListActions.js
+++ client/src/actions/ListActions.js
@@ -2,27 +2,27 @@ import apiClient from '../lib/ApiClient';
 import * as types from '../constants/ActionTypes';
 
 // list action creators
-export function createListRequest() {
-  return {
-    type: types.CREATE_LIST_REQUEST,
-  };
-}
+// export function createListRequest() {
+//   return {
+//     type: types.CREATE_LIST_REQUEST,
+//   };
+// }
 
 export function createListSuccess(list) {
   return {
     type: types.CREATE_LIST_SUCCESS,
-    payload: list,
+    list,
   };
 }
 
 export function createList(title, boardId, callback) {
   // thunk
   return function (dispatch) {
-    dispatch(createListRequest());
+    // dispatch(createListRequest());
 
     apiClient.createList(title, boardId, (data) => {
-      dispatch(createListSuccess(data.payload));
-      if (callback) callback(data.payload);
+      dispatch(createListSuccess(data.list));
+      if (callback) callback();
     });
   };
 }
